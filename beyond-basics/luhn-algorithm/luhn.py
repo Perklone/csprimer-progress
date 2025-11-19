5e6cce0750536b86088ea0c05efcfbd0917b1605
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2025-11-19 19:34:16 +0700

    video-accurate implementation of the luhn's algorithm
    I think turning my head around on doing the functional part, and
    inversing the validation for the value check is the hardest part for
    sure.

--- beyond-basics/luhn-algorithm/luhn.py
+++ beyond-basics/luhn-algorithm/luhn.py
@@ -1,14 +1,22 @@
 # Sample value from Wikipedia: 1789372997 (payload only)
 
-def verify(digits):
-    sum = 0
-    for index, digit in enumerate(digits[-2::-1]):
-        multiplied = int(digit) * (2 - index % 2)
-        sum += multiplied // 10 + multiplied % 10
+LOOKUPS = (
+    dict(zip('0123456789', (0,1,2,3,4,5,6,7,8,9))),
+    dict(zip('0123456789', (0,2,4,6,8,1,3,5,7,9))),
+)
 
-    result = (10 - (sum % 10))
-    return result == int(digits[-1])
+def verify_imperative(digits):
+    total = 0
+    for i, d in enumerate(reversed(digits)):
+        total += LOOKUPS[i%2][d]
+    
+    return total % 10 == 0
+
+def verify_functional(digits):
+    return sum(LOOKUPS[i%2][d] for i, d in enumerate(reversed(digits))) % 10 == 0
 
 if __name__ == '__main__':
-    assert verify("17893729974")
-    assert not verify("17893729975")
+    for verify in (verify_imperative, verify_functional):
+        assert verify("17893729974")
+        assert not verify("17893729975")
+        print(verify.__name__ + " ok")
